d92a2ffaeae0dfd1af6e16b438d254c8f70f6e0a
joecrosby <joseph.a.crosby@googlemail.com>
2021-09-12 19:12:34 +0100

    initial commit

---
+++ joe-spring-boot/src/main/java/com/example/joespringboot/JoeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.joespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoeSpringBootApplication.class, args);
+	}
+
+}
---
+++ joe-spring-boot/src/test/java/com/example/joespringboot/JoeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
